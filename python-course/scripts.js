c121eef50de105f040afe4de3376e3848ade3c4f
garata <giorgio.arata@consultant.com>
2020-10-02 15:28:29 +0200

    Add conditional structures example

--- python-course/scripts.js
+++ python-course/scripts.js
@@ -18,7 +18,7 @@ for (var i = 0; i < $ideBoxes.length; i++) {
 		$ideBoxes[i].insertBefore($ideFullscreenLnk, $ideBoxes[i].firstChild);
 	else
 		$ideBoxes[i].appendChild($ideFullscreenLnk);
-	console.log($ideFullscreenLnk);
+	console.log($ideBoxes[i]);
 }
 
 // map each section id to their corresponding navigation link
